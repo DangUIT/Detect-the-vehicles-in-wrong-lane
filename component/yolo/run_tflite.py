c833a8161ad13d1ef7fa7d0c0ea2904cb333286d
trandang <trandanganninh1@gmail.com>
2024-12-20 09:50:42 +0700

    Update organization

--- component/yolo/run_tflite.py
+++ component/yolo/run_tflite.py
@@ -3,12 +3,14 @@ from ultralytics import YOLO
 # Load the exported TFLite model
 # tflite_model = YOLO("train/weights/best.pt")
 
-tflite_model = YOLO("../../train/PTQ_416_736/best_full_integer_quant.tflite")
+# tflite_model = YOLO("../../train/PTQ_416_736/best_full_integer_quant.tflite")
 
+tflite_model = YOLO("../../train/PTQ_416_736/best_int8.tflite")
+# tflite_model = YOLO("../../train/PTQ_416_736/best_integer_quant.tflite")
 
 # tflite_model.info()
 
 # Run inference
-results = tflite_model("Test/Video/test3.mp4", save=True, show=True, imgsz=(480, 480))
+results = tflite_model("../../video/Video/test3.mp4", save=True, show=True, imgsz=(416, 736))
 
 
